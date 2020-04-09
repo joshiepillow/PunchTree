84b9937573c06604441ca5d61465727e727448b5
Joshiepillow <joshbce123@gmail.com>
2020-04-09 16:53:51 -0500

    ez

--- Assets/C# Scripts/PlayerAttack.cs
+++ Assets/C# Scripts/PlayerAttack.cs
@@ -6,11 +6,12 @@ public class PlayerAttack : MonoBehaviour
 {
     private Vector2 moveEach;
     private int speed;
+    private float time;
 
     public void Init(Vector2 clickPos) {
         gameObject.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Attack");
         moveEach = GlobalFunctions.findDirect(clickPos.x, clickPos.y);
-        speed = 1;
+        speed = 3;
     } 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,9 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(moveEach.x * speed * Time.deltaTime, moveEach.y * speed * Time.deltaTime, 0);
+        time += Time.deltaTime;
+        if (time > 1) {
+            Destroy(gameObject);
+        }
     }
 }
