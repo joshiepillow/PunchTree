cd850f325c33f8f8641ed1be75b8db4c7f24150a
Ruiqi Li <ruiqi.bce@outlook.com>
2020-04-09 17:07:39 -0500

    asdf

---
+++ Assets/C# Scripts/WorldSave.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class TileData
+{
+    string type;
+    int tile;
+    JsonData a;
+
+    public string GetMeta(string path)
+    {
+    }
+}
+
+public class RowData
+{
+    Dictionary<int, TileData> data;
+}
+
+
+public class WorldData
+{
+    List<TileData> tiles;
+}
+
+public class world
+{
+    WorldData w;
+    public world()
+    {
+        
+    }
+    
+    TileData GetTileData(int x, int y)
+    {
+
+    }
+}
