ed51394f72a06733ac67ab2137e7ea7721aafbd0
Joshiepillow <joshbce123@gmail.com>
2020-04-09 09:37:56 -0500

    f in the caht

---
+++ Assets/C# Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Debug.Log(x + " " + y); 
+
+        transform.position += new Vector3(x, y, 0);
+    }
+}
