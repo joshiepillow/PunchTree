98930ee9d839a89ab8dbbae98794f23955f6ba5a
Joshiepillow <joshbce123@gmail.com>
2020-04-09 14:44:15 -0500

    lul

---
+++ Assets/C# Scripts/GlobalFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFunctions : MonoBehaviour
+{
+    static Vector2 findDirect(float x, float y) {
+        float Hypotenuse = Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
+        return new Vector2(x/Hypotenuse, y/Hypotenuse);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/C# Scripts/Player.cs
+++ Assets/C# Scripts/Player.cs
@@ -14,13 +14,17 @@ public class Player : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        int x = (int) Input.GetAxis("Horizontal");
-        int y = (int) Input.GetAxis("Vertical");
-        Debug.Log(x + " " + y);
+        int x, y;
+        x = y = 0;
+        if (Input.GetKey(KeyCode.W)) y += 1;
+        if (Input.GetKey(KeyCode.S)) y -= 1;   
+        if (Input.GetKey(KeyCode.A)) x -= 1;
+        if (Input.GetKey(KeyCode.D)) x += 1;
+
+        //Debug.Log(x + " " + y);
         int newSpriteNum = 3*y + x + 4;
         if (newSpriteNum != spriteNumber && newSpriteNum != 4) {
             spriteNumber = newSpriteNum;
